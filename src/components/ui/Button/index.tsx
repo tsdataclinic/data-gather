f762584978a11b1962e8100065cad281334f0454
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-06-15 14:23:01 -0400

    Added basic textarea component

--- src/components/ui/Button/index.tsx
+++ src/components/ui/Button/index.tsx
@@ -1,17 +1,24 @@
+import classNames from 'classnames';
 import type { ReactNode, MouseEventHandler } from 'react';
 
 type Props = {
   children: ReactNode;
+  className?: string;
   onClick: MouseEventHandler<HTMLButtonElement>;
 };
 
-export default function Button({ children, onClick }: Props): JSX.Element {
+export default function Button({
+  children,
+  className,
+  onClick,
+}: Props): JSX.Element {
+  const buttonClassName = classNames(
+    className,
+    'py-2 px-4 text-white bg-blue-400 hover:bg-blue-500 active:bg-blue-400 rounded transition-colors',
+  );
+
   return (
-    <button
-      type="button"
-      className="py-2 px-4 text-white bg-blue-400 hover:bg-blue-500 active:bg-blue-400 rounded transition-colors"
-      onClick={onClick}
-    >
+    <button type="button" className={buttonClassName} onClick={onClick}>
       {children}
     </button>
   );
--- src/components/ui/TextArea/index.tsx
+++ src/components/ui/TextArea/index.tsx
@@ -4,45 +4,47 @@ import type { ChangeEvent, KeyboardEvent } from 'react';
 type Props = {
   className?: string;
   id?: string;
-  onChange: (val: string, event: ChangeEvent<HTMLInputElement>) => void;
+  onChange: (val: string, event: ChangeEvent<HTMLTextAreaElement>) => void;
 
   /**
    * Triggered when the 'Enter' key is pressed.
    */
-  onEnterPress?: (val: string, event: KeyboardEvent<HTMLInputElement>) => void;
+  onEnterPress?: (
+    val: string,
+    event: KeyboardEvent<HTMLTextAreaElement>,
+  ) => void;
   value: string;
 };
 
-export default function InputText({
+export default function TextArea({
   className,
   id,
   onChange,
   onEnterPress,
   value,
 }: Props): JSX.Element {
-  const onInputChange = (event: ChangeEvent<HTMLInputElement>): void => {
+  const onTextChange = (event: ChangeEvent<HTMLTextAreaElement>): void => {
     if (onChange) {
       onChange(event.currentTarget.value, event);
     }
   };
 
-  const onKeyPress = (event: KeyboardEvent<HTMLInputElement>): void => {
+  const onKeyPress = (event: KeyboardEvent<HTMLTextAreaElement>): void => {
     if (event.key === 'Enter' && onEnterPress) {
       onEnterPress(value, event);
     }
   };
 
-  const inputClassName = classNames(
+  const textareaClassName = classNames(
     className,
     'p-4 rounded-sm border border-gray-400',
   );
 
   return (
-    <input
+    <textarea
       id={id}
-      type="text"
-      className={inputClassName}
-      onChange={onInputChange}
+      className={textareaClassName}
+      onChange={onTextChange}
       onKeyPress={onKeyPress}
       value={value}
     />
