60bb02173d9ca4aa3ceb1d2c8711f40b6fc36c74
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-06-16 20:27:22 -0400

    Added a comment

--- src/components/ui/LabelWrapper/index.tsx
+++ src/components/ui/LabelWrapper/index.tsx
@@ -5,6 +5,10 @@ type Props = {
   label: string;
 };
 
+/**
+ * This is a lightweight component to just wrap a component with a
+ * label and add a little spacing.
+ */
 export default function LabelWrapper({ children, label }: Props): JSX.Element {
   return (
     <div>
