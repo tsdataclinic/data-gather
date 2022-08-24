a6c0eb77b803c7e975cb8ea063c90e1fdb34dd61
Izaak Baker <izaak@twosigmaventures.com>
2022-08-23 21:30:02 -0400

    Fix all the eslint stuff

---
+++ src/components/InterviewRunnerView/adapters.ts
@@ -0,0 +1,26 @@
+import * as Interview from '../../models/Interview';
+import * as InterviewScreen from '../../models/InterviewScreen';
+import { ScriptConfigSchema } from '../../script/ScriptConfigSchema';
+
+/**
+ * Attaches getter method necessary for ConfigurableScript to InterviewScreen.T type.
+ */
+export class InterviewScreenAdapter {
+  // eslint-disable-next-line
+  constructor(private interviewScreen: InterviewScreen.T) {}
+
+  getId(): string {
+    return this.interviewScreen.id;
+  }
+
+  public getInterviewScreen(): InterviewScreen.T {
+    return this.interviewScreen;
+  }
+}
+
+export function buildScriptConfig(_interview: Interview.T): ScriptConfigSchema {
+  return {
+    actions: {},
+    startingState: [],
+  };
+}
--- src/components/InterviewRunnerView/index.tsx
+++ src/components/InterviewRunnerView/index.tsx
@@ -1,15 +1,65 @@
-// import { useParams } from "react-router-dom";
-// import useInterview from "../../hooks/useInterview";
+import {
+  Interview as Engine,
+  Moderator,
+  ResponseConsumer,
+  ResponseData,
+} from '@dataclinic/interview';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import useInterview from '../../hooks/useInterview';
+import useInterviewScreens from '../../hooks/useInterviewScreens';
+import ConfigurableScript from '../../script/ConfigurableScript';
+import { ScriptConfigSchema } from '../../script/ScriptConfigSchema';
+import { buildScriptConfig, InterviewScreenAdapter } from './adapters';
+
 // import useInterviewConditionalActions from "../../hooks/useInterviewConditionalActions";
 // import useInterviewScreenEntries from "../../hooks/useInterviewScreenEntries";
-// import useInterviewScreens from "../../hooks/useInterviewScreens";
 
 export default function InterviewRunnerView(): JSX.Element | null {
-  // const { interviewId } = useParams();
-  // const interview = useInterview(interviewId ?? '');
-  // const screens = useInterviewScreens(interviewId ?? '');
+  const { interviewId } = useParams();
+  const interview = useInterview(interviewId ?? '');
+  const screens = useInterviewScreens(interviewId ?? '');
+  const [currentScreen, setCurrentScreen] =
+    useState<InterviewScreenAdapter | null>(null);
+  const [responseConsumer, setResponseConsumer] =
+    useState<ResponseConsumer | null>(null);
+
   // const entries = useInterviewScreenEntries(interviewId ?? '');
   // const actions = useInterviewConditionalActions(interviewId ?? '');
 
-  return <div>Runner!</div>;
+  if (!interview || !screens) {
+    throw new Error('Interview was not defined, or missing data');
+  }
+
+  useEffect(() => {
+    const indexedScreens: { [screenId: string]: InterviewScreenAdapter } = {};
+    screens.forEach(screen => {
+      indexedScreens[screen.id] = new InterviewScreenAdapter(screen);
+    });
+
+    const scriptConfig: ScriptConfigSchema = buildScriptConfig(interview);
+    const script: ConfigurableScript<InterviewScreenAdapter> =
+      new ConfigurableScript(scriptConfig, id => indexedScreens[id]);
+
+    const moderator: Moderator<InterviewScreenAdapter> = {
+      ask(consumer: ResponseConsumer, screen: InterviewScreenAdapter) {
+        setResponseConsumer(consumer);
+        setCurrentScreen(screen);
+      },
+    };
+
+    const engine: Engine<InterviewScreenAdapter> =
+      new Engine<InterviewScreenAdapter>(script, moderator);
+    engine.run((results: ResponseData) => {
+      console.log(results);
+    });
+  }, [interview, screens]);
+
+  return (
+    <>
+      <div>Runner!</div>
+      {currentScreen && <p>Current Screen is defined</p>}
+      {responseConsumer && <p>Response Consumer is defined</p>}
+    </>
+  );
 }
