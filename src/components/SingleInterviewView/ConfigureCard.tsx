5f789eafa6923a718c977944f349a16a7e85a69e
Brahm Capoor <brahmcapoor@gmail.com>
2022-09-05 22:41:06 -0400

    First set of fields for configuration card

--- src/components/SingleInterviewView/ConfigureCard.tsx
+++ src/components/SingleInterviewView/ConfigureCard.tsx
@@ -1,12 +1,14 @@
 import { faWrench } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import useInterviewScreens from '../../hooks/useInterviewScreens';
 import useInterviewStore from '../../hooks/useInterviewStore';
 import * as Interview from '../../models/Interview';
+// eslint-disable-next-line
 import Button from '../ui/Button';
 import Dropdown from '../ui/Dropdown';
 import LabelWrapper from '../ui/LabelWrapper';
+import TextArea from '../ui/TextArea';
 
 interface Props {
   interview: Interview.T;
@@ -19,6 +21,12 @@ function ConfigureCard({ interview }: Props): JSX.Element {
   const { startingState } = interview;
   const interviewStore = useInterviewStore();
   const screens = useInterviewScreens(interview.id);
+
+  const [displayedNotes, setDisplayedNotes] = useState(interview.notes);
+
+  const saveNotes = useCallback(async (): Promise<void> => {
+    await interviewStore.updateNotes(interview.id, displayedNotes);
+  }, [displayedNotes, interviewStore, interview]);
 
   const changeStartScreen = useCallback(
     async (index: number, screenId: string): Promise<void> => {
@@ -34,9 +42,8 @@ function ConfigureCard({ interview }: Props): JSX.Element {
     [interviewStore, interview],
   );
 
-  // TODO uncomment when I figure out why opening the dropdown
-  // triggers the button onclick
-
+  // TODO re-enable this when I figure out the dropdown triggering this
+  // eslint-disable-next-line
   const removeStartScreen = useCallback(
     async (index: number): Promise<void> => {
       await interviewStore.removeStartingScreen(interview.id, index);
@@ -58,16 +65,34 @@ function ConfigureCard({ interview }: Props): JSX.Element {
     }));
 
   return (
-    <div className="grid h-60 w-full grid-cols-4 bg-white p-8 shadow-md">
+    <div
+      className="grid h-60 w-full grid-cols-4 bg-white p-8 shadow-md"
+      style={{ height: 'auto' }}
+    >
       <div className="space-x-3">
         <FontAwesomeIcon size="1x" icon={faWrench} />
         <span>Configure</span>
       </div>
       <div className="col-span-3 space-y-4">
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <LabelWrapper
+          inline
+          label="Notes"
+          labelTextStyle={labelStyle}
+          inlineContainerStyles={{ verticalAlign: 'text-top' }}
+        >
+          <TextArea
+            onChange={setDisplayedNotes}
+            onEnterPress={saveNotes}
+            value={displayedNotes}
+          />
+        </LabelWrapper>
+
         <LabelWrapper
           inline
           label="Default Sequence"
           labelTextStyle={labelStyle}
+          inlineContainerStyles={{ verticalAlign: 'text-top' }}
         >
           {startingState.map((state, idx) => (
             <div
@@ -75,16 +100,16 @@ function ConfigureCard({ interview }: Props): JSX.Element {
               key={`startingstate_${state}_${idx}`}
               style={{ display: 'flex', marginBottom: '20px' }}
             >
+              {/* TODO this button's click handler is triggered when the dropdown below is opened */}
+              {/* <Button type="button" onClick={() => removeStartScreen(idx)}>
+                -
+              </Button> */}
               <Dropdown
                 onChange={screenId => changeStartScreen(idx, screenId)}
                 value={state}
                 options={getOptions()}
                 defaultButtonLabel=""
               />
-
-              <Button type="button" onClick={() => removeStartScreen(idx)}>
-                -
-              </Button>
             </div>
           ))}
           <Dropdown
