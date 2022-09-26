8a383390553e802c64e3297a30f7c9e9935ae6f2
Brahm Capoor <brahmcapoor@gmail.com>
2022-09-26 11:57:22 -0400

    Configuration card (#46)
    * Configuration card: setting up a default sequence
    
    * Added another TODO
    
    * First set of fields for configuration card
    
    * New Entry Modal & rudimentary screen card (#44)
    
    * init header card
    
    * init header card
    
    * add entry button working
    
    * sidebar display entry name instead of id
    
    * entry card displays information;
    
    * init header card
    
    * add entry button working
    
    * sidebar display entry name instead of id
    
    * entry card displays information;
    
    * pablos comments
    
    Co-authored-by: Tiffany Mei <tiffany@twosigmaventures.com>
    
    * Fix eslint (#45)
    
    Co-authored-by: Izaak Baker <izaak@twosigmaventures.com>
    
    * Configuration card: setting up a default sequence
    
    * Added another TODO
    
    * First set of fields for configuration card
    
    * Addressing feedback
    
    * Minor update to styling
    
    * Configuration card: setting up a default sequence
    
    * Added another TODO
    
    * First set of fields for configuration card
    
    * Addressing feedback
    
    * Configuration card: setting up a default sequence
    
    * First set of fields for configuration card
    
    * Minor update to styling
    
    * Addressed feedback
    
    Co-authored-by: tiffanymeits <101830016+tiffanymeits@users.noreply.github.com>
    Co-authored-by: Tiffany Mei <tiffany@twosigmaventures.com>
    Co-authored-by: Izaak Baker <izaak_baker@alumni.brown.edu>
    Co-authored-by: Izaak Baker <izaak@twosigmaventures.com>

--- src/components/SingleInterviewView/ConfigureCard.tsx
+++ src/components/SingleInterviewView/ConfigureCard.tsx
@@ -1,14 +1,149 @@
-import React from 'react';
+import { faWrench } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useCallback, useState } from 'react';
+import useInterviewScreens from '../../hooks/useInterviewScreens';
+import useInterviewStore from '../../hooks/useInterviewStore';
 import * as Interview from '../../models/Interview';
+import Button from '../ui/Button';
+import Dropdown from '../ui/Dropdown';
+import LabelWrapper from '../ui/LabelWrapper';
+import TextArea from '../ui/TextArea';
 
 interface Props {
   interview: Interview.T;
 }
 
+const labelStyle = { width: '10rem' };
+
 function ConfigureCard({ interview }: Props): JSX.Element {
+  const { startingState } = interview;
+  const interviewStore = useInterviewStore();
+  const screens = useInterviewScreens(interview.id);
+
+  const [displayedNotes, setDisplayedNotes] = useState(interview.notes);
+
+  const saveNotes = useCallback((): void => {
+    interviewStore.updateNotes(interview.id, displayedNotes);
+  }, [displayedNotes, interviewStore, interview]);
+
+  const changeStartScreen = useCallback(
+    (index: number, screenId: string): void => {
+      const newInterview = Interview.updateStartingScreen(
+        interview,
+        index,
+        screenId,
+      );
+      interviewStore.putInterview(newInterview);
+    },
+    [interviewStore, interview],
+  );
+
+  const addStartScreen = useCallback(
+    (screenId: string): void => {
+      const newInterview = Interview.addStartingScreen(interview, screenId);
+      interviewStore.putInterview(newInterview);
+    },
+    [interviewStore, interview],
+  );
+
+  const removeStartScreen = useCallback(
+    async (index: number): Promise<void> => {
+      const newInterview = Interview.removeStartingScreen(interview, index);
+      await interviewStore.putInterview(newInterview);
+    },
+    [interviewStore, interview],
+  );
+
+  if (!screens) {
+    return <p>No screens yet!</p>;
+  }
+
+  const getOptions = (): Array<{
+    displayValue: string;
+    value: string;
+  }> =>
+    screens.map(screen => ({
+      displayValue: screen.title,
+      value: screen.id,
+    }));
+
   return (
-    <div className="h-60 w-full bg-white shadow-md">
-      Configure Card for {interview.name}
+    <div
+      className="grid h-60 w-full grid-cols-4 bg-white p-8 shadow-md"
+      style={{ height: 'auto' }}
+    >
+      <div className="space-x-3">
+        <FontAwesomeIcon size="1x" icon={faWrench} />
+        <span>Configure</span>
+      </div>
+      <div className="col-span-3 space-y-4">
+        <LabelWrapper
+          inline
+          label="Notes"
+          labelTextStyle={labelStyle}
+          inlineContainerStyles={{ verticalAlign: 'text-top' }}
+        >
+          <TextArea
+            onChange={setDisplayedNotes}
+            onEnterPress={saveNotes}
+            value={displayedNotes}
+          />
+        </LabelWrapper>
+
+        <span>
+          <div
+            className="mt-5"
+            style={{
+              display: 'flex',
+            }}
+          >
+            <div className="w-40">Starting State</div>
+            <div
+              className="ml-4"
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+              }}
+            >
+              {startingState.map((state, idx) => (
+                <div
+                  // eslint-disable-next-line react/no-array-index-key
+                  key={`startingstate_${state}_${idx}`}
+                  style={{
+                    display: 'flex',
+                    marginBottom: '20px',
+                    alignItems: 'center',
+                  }}
+                >
+                  <span className="w-32">
+                    <Dropdown
+                      onChange={screenId => changeStartScreen(idx, screenId)}
+                      value={state}
+                      options={getOptions()}
+                      defaultButtonLabel=""
+                    />
+                  </span>
+
+                  <span className="ml-2">
+                    <Button
+                      type="button"
+                      onClick={() => removeStartScreen(idx)}
+                    >
+                      -
+                    </Button>
+                  </span>
+                </div>
+              ))}
+              <Dropdown
+                onChange={addStartScreen}
+                defaultButtonLabel="Add another screen!"
+                value={undefined} // TODO this value gets updated when a screen is selected, so the same screen can't be selected twice
+                options={getOptions()}
+              />
+            </div>
+          </div>
+        </span>
+      </div>
     </div>
   );
 }
