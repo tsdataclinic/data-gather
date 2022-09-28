aa07fee40c8122d34ea787181d67928dec68ff9e
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-09-28 16:57:19 -0400

    Reverted changes to EntryCard

--- src/components/SingleInterviewView/EntryCard.tsx
+++ src/components/SingleInterviewView/EntryCard.tsx
@@ -16,21 +16,21 @@ function EntryCard({ entry }: Props): JSX.Element {
   return (
     <ScrollableElement
       name={entry.id}
-      className="flex w-full flex-row bg-white p-5 shadow-md"
+      className="flex flex-row p-5 w-full bg-white shadow-md"
       key={entry.id}
     >
-      <div className="flex w-1/6 flex-row">
-        <FontAwesomeIcon className="h-6 w-6 pr-4" icon={faCircleQuestion} />
+      <div className="flex flex-row w-1/6">
+        <FontAwesomeIcon className="pr-4 w-6 h-6" icon={faCircleQuestion} />
         {entry.name}
       </div>
       <form className="flex flex-col gap-4" onSubmit={() => handleSubmit}>
         <div className="flex flex-row divide-x-2">
-          <div className="w-40 pr-4 text-right">Prompt</div>
+          <div className="pr-4 w-40 text-right">Prompt</div>
           <div className="pl-4">
             <div className="pb-2">
               <label htmlFor="fname">Text</label>
               <input
-                className="ml-4 rounded-lg border-2 border-solid p-1"
+                className="p-1 ml-4 rounded-lg border-2 border-solid"
                 type="text"
                 value={entry.prompt}
                 width={50}
@@ -40,7 +40,7 @@ function EntryCard({ entry }: Props): JSX.Element {
             <div className="pt-2">
               <label htmlFor="fname">Helper Text</label>
               <input
-                className="ml-4 rounded-lg border-2 border-solid p-1"
+                className="p-1 ml-4 rounded-lg border-2 border-solid"
                 type="text"
                 value={entry.text}
                 width={50}
@@ -50,12 +50,12 @@ function EntryCard({ entry }: Props): JSX.Element {
           </div>
         </div>
         <div className="flex flex-row divide-x-2">
-          <div className="w-40 pr-4 text-right">Response</div>
+          <div className="pr-4 w-40 text-right">Response</div>
           <div className="pl-4">
             <div className="pb-2">
               <label htmlFor="fname">ID</label>
               <input
-                className="ml-4 rounded-lg border-2 border-solid p-1"
+                className="p-1 ml-4 rounded-lg border-2 border-solid"
                 type="text"
                 value={entry.responseId}
                 width={50}
@@ -65,7 +65,7 @@ function EntryCard({ entry }: Props): JSX.Element {
             <div className="pt-2">
               <label htmlFor="fname">Type</label>
               <input
-                className="ml-4 rounded-lg border-2 border-solid p-1"
+                className="p-1 ml-4 rounded-lg border-2 border-solid"
                 type="text"
                 value={entry.responseType}
                 width={50}
