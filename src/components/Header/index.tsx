026ce276ace226aa305676913022dfaaeb5ee5c9
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-10-03 23:46:19 -0400

    navbar changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,15 +4,15 @@ import { Link } from 'react-router-dom';
 
 export default function Header(): JSX.Element {
   return (
-    <header className="flex items-center py-2 px-8 w-full h-16 text-white bg-slate-900">
+    <header className="z-50 flex h-12 w-full items-center bg-slate-800 py-2 px-8 text-white">
       <div className="flex-1">
         <Link to="/">
-          <span className="text-2xl font-bold">Interview</span>
-          <div>by data clinic</div>
+          <span className="text-2xl font-bold tracking-wide">Interview</span>
+          <span className="pl-2 text-xs">by Data Clinic</span>
         </Link>
       </div>
       <div>
-        <FontAwesomeIcon size="2x" icon={faUser} />
+        <FontAwesomeIcon size="lg" icon={faUser} />
       </div>
     </header>
   );
