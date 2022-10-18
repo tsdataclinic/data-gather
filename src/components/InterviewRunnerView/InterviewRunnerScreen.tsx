90798c177bd35a4fae44a99ab5d199b5547cfc56
Izaak Baker <izaak@twosigmaventures.com>
2022-10-18 01:03:50 -0400

    Factor out runner screen into its own component, switch on type

---
+++ src/components/InterviewRunnerView/InterviewRunnerScreen.tsx
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { useCallback } from 'react';
+import { ResponseConsumer, ResponseData } from '@dataclinic/interview';
+import * as InterviewScreen from '../../models/InterviewScreen';
+import * as InterviewScreenEntry from '../../models/InterviewScreenEntry';
+import Form from '../ui/Form';
+
+type Props = {
+  entries: Map<string, InterviewScreenEntry.T[]>;
+  responseConsumer: ResponseConsumer;
+  responseData: ResponseData;
+  screen: InterviewScreen.T;
+};
+
+export default function InterviewRunnerScreen({
+  screen,
+  entries,
+  responseConsumer,
+}: Props): JSX.Element {
+  const handleSubmit = useCallback(
+    (formData: Map<string, string>) => {
+      if (!responseConsumer) {
+        return;
+      }
+      responseConsumer.answer(Object.fromEntries(formData));
+      responseConsumer.submit();
+    },
+    [responseConsumer],
+  );
+
+  return (
+    <>
+      <h1>{screen.title}</h1>
+      {screen.headerText && <h3>{screen.headerText}</h3>}
+      <Form onSubmit={handleSubmit}>
+        {entries.get(screen.id)?.map((entry: InterviewScreenEntry.T) => {
+          switch (entry.responseType) {
+            case 'text':
+              return (
+                <Form.Input
+                  key={entry.id}
+                  name={entry.responseId}
+                  label={entry.prompt}
+                />
+              );
+            // TODO: add 'number' and 'boolean' inputs.
+            default:
+              return (
+                <div>
+                  <em>
+                    Response type &quot;{entry.responseType}&quot; not
+                    implemented.
+                  </em>
+                </div>
+              );
+          }
+        })}
+        <Form.SubmitButton />
+      </Form>
+    </>
+  );
+}
