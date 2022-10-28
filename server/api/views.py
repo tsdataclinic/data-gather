e5cf51aebee0f050aaeeb7272867c06d7dde0b3c
mrf <mrf@XPS13935032423378.verizon.net>
2022-10-28 20:02:58 +0000

    added get endpoint

--- server/api/views.py
+++ server/api/views.py
@@ -1,15 +1,34 @@
 from fastapi import FastAPI
 
-from server.api.schemas import PydanticInterview
+from schemas import PydanticInterview
+from server.models import Interview
+
+from sqlalchemy import create_engine
+
+from sqlalchemy.orm import Session, sessionmaker
+
 
 app = FastAPI(title="Interview App API")
 
 
-@app.get("/api/health")
-def health_check():
-    return {"message": "API is up!"}
+@app.get("/")
+def hello_api():
+    return {"message": "Hello World"}
 
 
 @app.post("/api/interviews/", response_model=PydanticInterview)
 def create_interview(interview: PydanticInterview):
     return "whatever"
+
+    # https://fastapi.tiangolo.com/tutorial/sql-databases/
+    # create_the_engine()
+    # engine.commit()
+
+
+@app.get("/api/interviews/")
+def get_interviews():
+    engine = create_engine("sqlite:////tmp/db.db")
+    SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+    interviews = [i.__dict__ for i in SessionLocal().query(Interview).limit(100).all()]
+    return interviews
