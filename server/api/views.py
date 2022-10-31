121e4751c93831a278fe489ee99acf6b6cf97c7b
Indraneel Purohit <indraneel.purohit@twosigma.com>
2022-10-31 14:12:22 -0400

    start airtable server code

--- server/api/views.py
+++ server/api/views.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from airtable_api import * 
 
 app = FastAPI(title="Interview App API")
 
 @app.get("/")
 def hello_api():
     return {"message": "Hello World"}
+
+
+@app.get("/airtable-records")
+def get_all_airtable_records():
+    pass
+
+@app.get("/airtable-record")
+def get_airtable_record():
+    pass
+
+@app.post("/airtable-record")
+def create_airtable_record():
+    pass
+
+@app.put("/airtable-record")
+def update_airtable_record():
+    pass
+
