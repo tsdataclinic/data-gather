f440416add616e2dfc0508ea3e751c634b1e75ca
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-11-01 14:37:42 -0400

    Added GET for single interview and multiple interviews, including nested screens

--- server/api/views.py
+++ server/api/views.py
@@ -1,10 +1,10 @@
 from typing import List
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from sqlmodel import Session, create_engine
 
-from server.models import Interview
+from server.models import Interview, InterviewGetWithScreens
 from server.models_util import SQLITE_DB_PATH
 
 app = FastAPI(title="Interview App API")
@@ -30,12 +30,29 @@ def create_interview(interview: Interview):
     return "success"
 
 
-@app.get("/api/interviews/", response_model=List[Interview], tags=["interviews"])
+@app.get(
+    "/api/interviews/{interview_id}",
+    response_model=InterviewGetWithScreens,
+    tags=["interviews"],
+)
+def get_interview(interview_id: str) -> Interview:
+    engine = create_engine(f"sqlite:///{SQLITE_DB_PATH}")
+    session = Session(autocommit=False, autoflush=False, bind=engine)
+    interview = session.get(Interview, interview_id)
+    if not interview:
+        raise HTTPException(status_code=404, detail="Interview not found")
+    return interview
+
+
+@app.get(
+    "/api/interviews/",
+    response_model=List[Interview],
+    tags=["interviews"],
+)
 def get_interviews() -> list[Interview]:
     engine = create_engine(f"sqlite:///{SQLITE_DB_PATH}")
     session = Session(autocommit=False, autoflush=False, bind=engine)
     interviews: List[Interview] = session.query(Interview).limit(100).all()
-
     first_interview = interviews[0]
     print(first_interview)
     print(first_interview.screens)
