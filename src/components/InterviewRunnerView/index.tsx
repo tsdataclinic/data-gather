84beb7ef6bb4ee5a4975f24d41dc6270887aad7a
Izaak Baker <izaak@twosigmaventures.com>
2022-11-03 00:36:46 -0400

    Drive configurablescript with frontend types

--- src/components/InterviewRunnerView/index.tsx
+++ src/components/InterviewRunnerView/index.tsx
@@ -6,13 +6,13 @@ import {
 } from '@dataclinic/interview';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { buildScriptConfig, InterviewScreenAdapter } from './adapters';
 import useInterview from '../../hooks/useInterview';
 import useInterviewScreenEntries from '../../hooks/useInterviewScreenEntries';
 import useInterviewScreens from '../../hooks/useInterviewScreens';
-import ConfigurableScript from '../../script/ConfigurableScript';
-import { ScriptConfigSchema } from '../../script/ScriptConfigSchema';
 import InterviewRunnerScreen from './InterviewRunnerScreen';
+import useInterviewConditionalActions from '../../hooks/useInterviewConditionalActions';
+import * as InterviewScreen from '../../models/InterviewScreen';
+import ConfigurableScript from '../../script/ConfigurableScript';
 
 /**
  * Runs an interview based on the ID of the interview in the URL params.
@@ -21,8 +21,10 @@ export default function InterviewRunnerView(): JSX.Element | null {
   const { interviewId } = useParams();
   const interview = useInterview(interviewId);
   const screens = useInterviewScreens(interviewId);
-  const [currentScreen, setCurrentScreen] =
-    useState<InterviewScreenAdapter | null>(null);
+  const actions = useInterviewConditionalActions(interviewId);
+  const [currentScreen, setCurrentScreen] = useState<InterviewScreen.T | null>(
+    null,
+  );
   const [responseConsumer, setResponseConsumer] =
     useState<ResponseConsumer | null>(null);
   const [responseData, setResponseData] = useState<ResponseData>({});
@@ -31,28 +33,28 @@ export default function InterviewRunnerView(): JSX.Element | null {
 
   // Construct and run an interview on component load.
   useEffect(() => {
-    if (!interview || !screens) {
+    if (!interview || !screens || !actions) {
       return;
     }
 
     // Load screens for interview and index them by their ID
-    const indexedScreens: { [screenId: string]: InterviewScreenAdapter } = {};
-    screens.forEach(screen => {
-      indexedScreens[screen.id] = new InterviewScreenAdapter(screen);
-    });
+    const indexedScreens: Map<string, InterviewScreen.T> = new Map();
+    screens.forEach(screen => indexedScreens.set(screen.id, screen));
 
     // Create a script from the interview definition
     // TODO: Also include actions in the script creation
-    const scriptConfig: ScriptConfigSchema = buildScriptConfig(interview);
-    const script: ConfigurableScript<InterviewScreenAdapter> =
-      new ConfigurableScript(scriptConfig, id => indexedScreens[id]);
+    const script: ConfigurableScript = new ConfigurableScript(
+      interview,
+      actions,
+      indexedScreens,
+    );
 
     // Moderator, when prompted to ask, will set state on this component so that it will
     // display the correct screen.
-    const moderator: Moderator<InterviewScreenAdapter> = {
+    const moderator: Moderator<InterviewScreen.T> = {
       ask(
         consumer: ResponseConsumer,
-        screen: InterviewScreenAdapter,
+        screen: InterviewScreen.T,
         data: ResponseData,
       ) {
         setResponseConsumer(consumer);
@@ -62,13 +64,15 @@ export default function InterviewRunnerView(): JSX.Element | null {
     };
 
     // Build interview from script and moderator, and kick it off.
-    const engine: Engine<InterviewScreenAdapter> =
-      new Engine<InterviewScreenAdapter>(script, moderator);
+    const engine: Engine<InterviewScreen.T> = new Engine<InterviewScreen.T>(
+      script,
+      moderator,
+    );
     engine.run((result: ResponseData) => {
       setResponseData(result);
       setComplete(true);
     });
-  }, [interview, screens]);
+  }, [interview, screens, actions]);
 
   return (
     <div>
@@ -89,17 +93,14 @@ export default function InterviewRunnerView(): JSX.Element | null {
         </div>
       ) : (
         <div>
-          {currentScreen &&
-            currentScreen?.getInterviewScreen() &&
-            entries &&
-            responseConsumer && (
-              <InterviewRunnerScreen
-                screen={currentScreen.getInterviewScreen()}
-                entries={entries}
-                responseData={responseData}
-                responseConsumer={responseConsumer}
-              />
-            )}
+          {currentScreen && currentScreen && entries && responseConsumer && (
+            <InterviewRunnerScreen
+              screen={currentScreen}
+              entries={entries}
+              responseData={responseData}
+              responseConsumer={responseConsumer}
+            />
+          )}
         </div>
       )}
     </div>
