1d28ae4ae605e3f79c8ea93d8399ea309d3f9c3c
kbiggers <krbiggers@gmail.com>
2022-11-18 16:43:22 -0500

    Adding update screen endpoint (#98)
    * fixed incorrect List typing
    
    * Added endpoint for creating a new screen
    
    * WIP: Adding actions and entities to populdated data
    
    * Adding conditional actions and screen entries to pre-populated database data
    
    * WIP: Adding update screen endpoint to API
    
    * Adding update screen endpoint to API
    
    * Refactored how we are updating screen with requested update
    
    Co-authored-by: mrf <mrf@XPS13935032423378.verizon.net>

--- server/api/views.py
+++ server/api/views.py
@@ -1,14 +1,26 @@
+import copy
+import logging
+
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from sqlmodel import Session
-from sqlalchemy.exc import IntegrityError
+from sqlmodel import Session, SQLModel
+from sqlalchemy.exc import IntegrityError, NoResultFound
 
 from server.init_db import SQLITE_DB_PATH
-from server.models import Interview, InterviewScreen
+from server.models import (
+    Interview,
+    InterviewScreen,
+    InterviewScreenEntry,
+    ConditionalAction,
+    OrderedModel,
+)
 from server.models_util import prepare_relationships
 from server.engine import create_fk_constraint_engine
 
 from server.api.exceptions import InvalidOrder
+
+LOG = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
 
 app = FastAPI(title="Interview App API")
 
@@ -61,7 +73,7 @@ def get_interviews() -> list[Interview]:
 # TODO: this needs to return conditional actions and entries as well
 @app.get(
     "/api/interviewScreens/{screen_id}",
-    response_model=prepare_relationships(InterviewScreen),
+    response_model=prepare_relationships(InterviewScreen, ["actions", "entries"]),
     tags=["InterviewScreens"],
 )
 def get_interview_screen(screen_id: str) -> InterviewScreen:
@@ -106,6 +118,133 @@ def create_interview_screen(screen: InterviewScreen) -> InterviewScreen:
         session.refresh(screen)
 
         return screen
+
+@app.put(
+    "/api/interviewScreens/{screen_id}",
+    response_model=prepare_relationships(InterviewScreen, ["actions", "entries"]),
+    tags=["InterviewScreens"],
+)
+def update_interview_screen(
+    screen_id: str,
+    screen: prepare_relationships(InterviewScreen, ["actions", "entries"]),
+) -> InterviewScreen:
+    engine = create_fk_constraint_engine(SQLITE_DB_PATH)
+    with Session(autocommit=False, autoflush=False, bind=engine) as session:
+        try:
+            db_screen = (
+                session.query(InterviewScreen).where(InterviewScreen.id == screen_id).one()
+            )
+        except NoResultFound:
+            raise HTTPException(
+                status_code=404, detail=f"Screen with id {screen_id} not found"
+            )
+        
+        _validate_sequential_order(screen.actions)
+        _validate_sequential_order(screen.entries)
+        
+        casted_request_screen = _cast_correct_types(screen)
+
+        for rel, primary_key in zip(["actions", "entries"], ["id", "response_key"]):
+            session, db_screen = _update_db_screen_relationships(
+                session,
+                db_screen,
+                casted_request_screen,
+                rel,
+                primary_key,
+            )
+
+        session.add(db_screen)
+
+        LOG.info(f"Making changes to screen:")
+        LOG.info(f"Additions {session.new}" )
+        LOG.info(f"Deletions: {session.deleted}")
+        LOG.info(f"Updates: {session.dirty}")
+        
+        try:
+            session.commit()
+        except IntegrityError as e:
+            raise HTTPException(
+                status_code=400,
+                detail=str(e.orig),
+            )
+        return screen
+
+
+def _update_model_diff(existing_model: SQLModel, new_model: SQLModel):
+    """Update a model returned from the DB with any changes in the new model"""
+    for key, val in new_model.dict().items():
+        if getattr(existing_model, key) != val:
+            setattr(existing_model, key, val)
+    
+    return existing_model
+
+def _cast_correct_types(request_screen: InterviewScreen):
+    """Cast the relationship models on the request to the appropriate types"""
+    casted_screen = copy.deepcopy(request_screen)
+    casted_screen.actions = [ConditionalAction(**action.dict()) for action in request_screen.actions]
+    casted_screen.entries = [InterviewScreenEntry(**entry.dict()) for entry in request_screen.entries]
+    return casted_screen
+
+
+def _update_db_screen_relationships(
+    session: Session, 
+    db_screen: InterviewScreen,
+    request_screen: InterviewScreen,
+    relationship: str,
+    rel_primary_key: str,
+):
+    """
+    Handle all necessary adds, deletes and updates to the DB session 
+    for the given screens and relationship.
+    
+    Args:
+        session: DB session 
+        db_screen: The InterviewScreen as read from the DB
+        request_screen: The InterviewScreen sent in the request body
+        relationship: The screen relationship to target for updates
+        rel_primary_key: The primary key to reference on the given relationship object
+
+    """
+    request_models = {
+        getattr(model, rel_primary_key): model
+        for model in getattr(request_screen, relationship)
+    }
+
+    db_model_ids = []
+    for i, model in enumerate(getattr(db_screen, relationship)):
+        model_id = getattr(model, rel_primary_key)
+        db_model_ids.append(model_id)
+        if request_models.get(model_id) is None:
+            session.delete(model)
+        else:
+            updated_model = _update_model_diff(model, request_models.get(model_id))
+            getattr(db_screen, relationship)[i] = updated_model
+
+    for id, model in request_models.items():
+        if id not in db_model_ids:
+            session.add(model)
+    
+    return session, db_screen
+
+
+def _validate_sequential_order(request_models: list[OrderedModel]):
+    """Validate that the provided ordered models are in sequential order starting at 1"""
+    model_order = sorted(
+        [
+            i.order
+            for i in request_models
+        ]
+    )
+    exc = HTTPException(
+            status_code=400,
+            detail=f"Invalid order provided for added/updated models {model_order}",
+        )
+
+    if model_order != list(range(min(model_order), max(model_order) + 1)):
+        raise exc
+
+    if model_order[0] != 1:
+        raise exc
 
 
 def _adjust_screen_order(
--- server/models.py
+++ server/models.py
@@ -1,9 +1,9 @@
 import logging
-from typing import List, Optional
+from typing import Optional
 
 import uuid
 
-from sqlmodel import Field, Relationship, UniqueConstraint
+from sqlmodel import Field, Relationship
 
 from server.models_util import APIModel
 
@@ -20,31 +20,36 @@ class Interview(APIModel, table=True):
     notes: str
 
     # relationships
-    screens: List["InterviewScreen"] = Relationship(back_populates="interview")
+    screens: list["InterviewScreen"] = Relationship(back_populates="interview")
 
 
-class InterviewScreen(APIModel, table=True):
+class OrderedModel(APIModel):
+    order: int
+
+
+class InterviewScreen(OrderedModel, table=True):
     __tablename__: str = "interview_screen"
     order: int = 0
     header_text: str
-    id: uuid.UUID = Field(default_factory=uuid.uuid4, primary_key=True)
+    id: Optional[uuid.UUID] = Field(
+        default_factory=uuid.uuid4, primary_key=True, nullable=False
+    )
     interview_id: str = Field(foreign_key="interview.id")
     title: str
     starting_state_order: int = 0
 
     # relationships
-    actions: List["ConditionalAction"] = Relationship(back_populates="screen")
-    entries: List["InterviewScreenEntry"] = Relationship(back_populates="screen")
+    actions: list["ConditionalAction"] = Relationship(back_populates="screen")
+    entries: list["InterviewScreenEntry"] = Relationship(back_populates="screen")
     interview: Interview = Relationship(back_populates="screens")
 
 
-class InterviewScreenEntry(APIModel, table=True):
+class InterviewScreenEntry(OrderedModel, table=True):
     __tablename__: str = "interview_screen_entry"
     name: str
     prompt: str
     response_key: str = Field(primary_key=True)
-    screen_id: str = Field(foreign_key="interview_screen.id")
-    order: int
+    screen_id: uuid.UUID = Field(foreign_key="interview_screen.id")
     response_type: str
     text: str
 
@@ -52,14 +57,14 @@ class InterviewScreenEntry(APIModel, table=True):
     screen: InterviewScreen = Relationship(back_populates="entries")
 
 
-class ConditionalAction(APIModel, table=True):
+class ConditionalAction(OrderedModel, table=True):
     __tablename__: str = "conditional_action"
     action_payload: str
     action_type: str
     id: str = Field(primary_key=True)
     order: int
     response_key: str
-    screen_id: str = Field(foreign_key="interview_screen.id")
+    screen_id: uuid.UUID = Field(foreign_key="interview_screen.id")
     value: str
 
     # relationships
