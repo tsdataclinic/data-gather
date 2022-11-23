5b4b6d8caa285d6b37974c0604cde38f0a579124
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-11-23 14:02:29 -0500

    Added new api models for frontend

---
+++ src/api/models/InterviewScreenEntryBase.ts
@@ -0,0 +1,20 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { ResponseType } from './ResponseType';
+
+/**
+ * Any models that are returned in our REST API should extend this class.
+ * This class handles any snake_case to camelCase conversions.
+ */
+export type InterviewScreenEntryBase = {
+  order: number;
+  id: string;
+  name: string;
+  prompt: string;
+  responseKey: string;
+  screenId: string;
+  responseType: ResponseType;
+  text: string;
+};
