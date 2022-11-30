26dc083b74fa78cd68fcf9aec0e25c9a5134737b
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-11-29 23:32:24 -0500

    Added documentation

--- src/hooks/useInterviewMutation.ts
+++ src/hooks/useInterviewMutation.ts
@@ -6,6 +6,30 @@ import {
 import useInterviewStore from './useInterviewStore';
 import type { InterviewServiceAPI } from '../services/InterviewService/InterviewServiceAPI';
 
+/**
+ * This hook should be used to execute any API calls that need to mutate the
+ * server-side interview state. This hook wraps react-query's `useMutation`
+ * hook to make it easy to call an InterviewService API function and then
+ * invalidate any necessary queries that react-query might need to refetch.
+ *
+ * Usage:
+ *   const updateInterview = useInterviewMutation({
+ *     mutation: (interview, api) =>
+ *       api.interviewAPI.updateInterview(interview),
+ *     invalidateQuery: ['interview', interview.id]
+ *   });
+ *
+ *   ...
+ *   const onSave = () => updateInterview(interview);
+ *
+ * You can also pass an `onSuccess` and `onError` function to the mutation
+ * function in case there's any post-mutation logic you need, like toast
+ * messages.
+ *   const onSave = () => updateInterview(interview, {
+ *     onSuccess: () => toaster.notifySuccess('Success!', 'Interview saved'),
+ *     onError: error => console.error(error),
+ *   });
+ */
 export default function useInterviewMutation<
   TVariables = unknown,
   TResponse = unknown,
