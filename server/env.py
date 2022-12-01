e27e8d680f4db9f4f1b218b7b0fe17377d8189db
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2022-11-30 19:45:23 -0500

    Use dotenv library to load env vars into python server (#104)

---
+++ server/env.py
@@ -0,0 +1,29 @@
+import os
+from typing import Union
+
+from dotenv import find_dotenv, load_dotenv
+
+ENV_FILES = [
+    ".env",
+    ".env.local",
+    ".env.development.local",
+    ".env.test.local",
+    ".env.production.local",
+]
+
+
+def load_env():
+    env_path = ""
+    for env_test_path in ENV_FILES:
+        path = find_dotenv(env_test_path)
+        if path:
+            env_path = path
+            break
+    load_dotenv(env_path)
+
+
+load_env()
+
+
+def get_env(env_key: str) -> Union[str, None]:
+    return os.environ.get(env_key)
