cfa23e4797a59d3a79d914d4a0146c20009d9277
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2023-01-03 14:53:36 -0500

    Add scroll area

--- src/components/SingleInterviewView/ConfigurePage/index.tsx
+++ src/components/SingleInterviewView/ConfigurePage/index.tsx
@@ -1,6 +1,7 @@
 import * as Interview from '../../../models/Interview';
 import ConfigureCard from './ConfigureCard';
 import OnSubmitCard from './OnSubmitCard';
+import ScrollArea from '../../ui/ScrollArea';
 
 type Props = {
   interview: Interview.WithScreensT;
@@ -8,9 +9,11 @@ type Props = {
 
 export default function ConfigurePage({ interview }: Props): JSX.Element {
   return (
-    <div className="w-full space-y-8 p-14">
-      <ConfigureCard interview={interview} />
-      <OnSubmitCard interview={interview} />
-    </div>
+    <ScrollArea className="w-full overflow-auto">
+      <div className="space-y-8 p-14">
+        <ConfigureCard interview={interview} />
+        <OnSubmitCard interview={interview} />
+      </div>
+    </ScrollArea>
   );
 }
