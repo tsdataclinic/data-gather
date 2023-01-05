5041828b15bdb5f1c94bb2813b11c534116176b3
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2023-01-05 07:51:48 -0500

    Add airtable client back

--- server/api/views.py
+++ server/api/views.py
@@ -18,31 +18,23 @@ from server.engine import create_fk_constraint_engine
 from server.init_db import SQLITE_DB_PATH
 from server.models.common import OrderedModel
 from server.models.conditional_action import ConditionalAction
-from server.models.interview import (
-    Interview,
-    InterviewCreate,
-    InterviewRead,
-    InterviewReadWithScreensAndActions,
-    InterviewUpdate,
-    ValidationError,
-)
-from server.models.interview_screen import (
-    InterviewScreen,
-    InterviewScreenCreate,
-    InterviewScreenRead,
-    InterviewScreenReadWithChildren,
-    InterviewScreenUpdate,
-)
+from server.models.interview import (Interview, InterviewCreate, InterviewRead,
+                                     InterviewReadWithScreensAndActions,
+                                     InterviewUpdate, ValidationError)
+from server.models.interview_screen import (InterviewScreen,
+                                            InterviewScreenCreate,
+                                            InterviewScreenRead,
+                                            InterviewScreenReadWithChildren,
+                                            InterviewScreenUpdate)
 from server.models.interview_screen_entry import (
-    InterviewScreenEntry,
-    InterviewScreenEntryReadWithScreen,
-)
+    InterviewScreenEntry, InterviewScreenEntryReadWithScreen)
 from server.models.submission_action import SubmissionAction
 from server.models.user import User, UserRead
 
 LOG = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
+airtable_client = AirtableAPI(AIRTABLE_API_KEY, AIRTABLE_BASE_ID)
 
 class Settings(BaseSettings):
     BACKEND_CORS_ORIGINS: list[Union[str, AnyHttpUrl]] = ["http://localhost:3000"]
