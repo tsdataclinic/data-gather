f11afbf2d2dcf18b6348f16e113aa6ca244f0574
Indraneel Purohit <indraneel.purohit@twosigma.com>
2023-01-04 20:20:22 -0500

    initial UI for airtable column name in conditional action editor (#135)
    * initial UI for airtable column name in conditional action editor
    
    - stores the airtable column name as a new variable `responseKeyColumn` on the `ConditionalAction` model
    - doesn't yet update automatically as Airtable fields list is edited, but does update on Save -> refresh
    
    * add datepicker to conditional action input
    
    * Update AirtableFieldSelector.tsx
    
    reset selectedFields when selecting new Airtable table in EntryCard

--- src/components/SingleInterviewView/ActionCard/index.tsx
+++ src/components/SingleInterviewView/ActionCard/index.tsx
@@ -1,9 +1,14 @@
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+
 import * as React from 'react';
 import * as IconType from '@fortawesome/free-solid-svg-icons';
 import { faLocationArrow } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { MixedCheckbox } from '@reach/checkbox';
 import { Element as ScrollableElement } from 'react-scroll';
+import { Calendar } from 'primereact/calendar';
 import * as ConditionalAction from '../../../models/ConditionalAction';
 import * as Interview from '../../../models/Interview';
 import Dropdown from '../../ui/Dropdown';
@@ -35,6 +40,62 @@ type Props = {
   onActionDelete: (actionToDelete: EditableAction) => void;
 };
 
+// The conditionalOperatorRow doesn't use Form.Input or other Form
+// subcomponents because we need more control over how it renders
+function ConditionalOperatorRow({
+  action,
+  onResponseKeyChange,
+  allResponseKeyOptions,
+  onResponseKeyColumnChange,
+  allResponseKeyColumnOptions,
+  onConditionalOperatorChange,
+  onConditionalValueChange,
+  conditionalValueType,
+}: any): JSX.Element {
+  return (
+    <div className="flex items-center space-x-4">
+      <p className="w-20">Condition</p>
+      <Dropdown
+        onChange={onResponseKeyChange}
+        placeholder="Response variable"
+        value={action.responseKey}
+        options={allResponseKeyOptions}
+      />
+      {/* TODO - connect up to `entry` state object and condition on ResponseType.AIRTABLE instead of this approach */}
+      {allResponseKeyColumnOptions.length > 0 && (
+        <Dropdown
+          onChange={onResponseKeyColumnChange}
+          placeholder="Response column variable"
+          value={action.responseKeyColumn}
+          options={allResponseKeyColumnOptions}
+        />
+      )}
+
+      <Dropdown
+        onChange={onConditionalOperatorChange}
+        placeholder="Operator"
+        value={action.conditionalOperator}
+        options={OPERATOR_OPTIONS}
+      />
+
+      {/* TODO - connect up to `entry` state object and condition on ResponseType.AIRTABLE instead of this approach */}
+      {conditionalValueType === 'date' ? (
+        <Calendar
+          placeholder="value"
+          onChange={e => onConditionalValueChange(e.value)}
+          value={action.value ?? ''}
+        />
+      ) : (
+        <InputText
+          placeholder="value"
+          onChange={onConditionalValueChange}
+          value={action.value ?? ''}
+        />
+      )}
+    </div>
+  );
+}
+
 function ActionCard(
   { action, interview, onActionChange, onActionDelete }: Props,
   forwardedRef: React.ForwardedRef<HTMLFormElement>,
@@ -48,6 +109,10 @@ function ActionCard(
     action.conditionalOperator ===
       ConditionalAction.ConditionalOperator.ALWAYS_EXECUTE,
   );
+
+  const [conditionalValueType, setConditionalValueType] = React.useState<
+    'text' | 'date'
+  >(action.value && Date.parse(action.value) ? 'date' : 'text');
 
   const onAlwaysExecuteChange = React.useCallback(
     (event: React.ChangeEvent<HTMLInputElement>): void => {
@@ -90,6 +155,16 @@ function ActionCard(
     [action, onActionChange],
   );
 
+  const onResponseKeyColumnChange = React.useCallback(
+    (newResponseKeyColumn: string) => {
+      onActionChange(action, {
+        ...action,
+        responseKeyColumn: newResponseKeyColumn,
+      });
+    },
+    [action, onActionChange],
+  );
+
   const onConditionalValueChange = React.useCallback(
     (newValue: string) => {
       onActionChange(action, {
@@ -122,32 +197,21 @@ function ActionCard(
         })
       : [];
 
-  // The conditionalOperatorRow doesn't use Form.Input or other Form
-  // subcomponents because we need more control over how it renders
-  const conditionalOperatorRow = isAlwaysExecuteChecked ? null : (
-    <div className="flex items-center space-x-4">
-      <p className="w-20">Condition</p>
-      <Dropdown
-        onChange={onResponseKeyChange}
-        placeholder="Response variable"
-        value={action.responseKey}
-        options={allResponseKeyOptions}
-      />
-
-      <Dropdown
-        onChange={onConditionalOperatorChange}
-        placeholder="Operator"
-        value={action.conditionalOperator}
-        options={OPERATOR_OPTIONS}
-      />
-
-      <InputText
-        placeholder="value"
-        onChange={onConditionalValueChange}
-        value={action.value ?? ''}
-      />
-    </div>
-  );
+  // TODO: have to connect <ActionCard> to `entry` for responseKeyColumns to change before 'Save' is clicked
+  const allResponseKeyColumnOptions =
+    interviewScreens && screenEntriesMap
+      ? interviewScreens.flatMap(screen => {
+          const entries = screenEntriesMap.get(screen.id) || [];
+          return entries
+            .filter(entry => entry.responseType === 'airtable')
+            .flatMap(entry =>
+              entry.responseTypeOptions.selectedFields.map(field => ({
+                displayValue: `${entry.responseTypeOptions.selectedBase} - ${field}`,
+                value: field,
+              })),
+            );
+        })
+      : [];
 
   return (
     <ScrollableElement
@@ -177,7 +241,26 @@ function ActionCard(
             onChange={onAlwaysExecuteChange}
           />
         </LabelWrapper>
-        {conditionalOperatorRow}
+        <LabelWrapper inline labelAfter label="Compare against a date">
+          <MixedCheckbox
+            onChange={e =>
+              setConditionalValueType(e.target.checked ? 'date' : 'text')
+            }
+            checked={conditionalValueType === 'date'}
+          />
+        </LabelWrapper>
+        {!isAlwaysExecuteChecked && (
+          <ConditionalOperatorRow
+            action={action}
+            onResponseKeyChange={onResponseKeyChange}
+            allResponseKeyOptions={allResponseKeyOptions}
+            onResponseKeyColumnChange={onResponseKeyColumnChange}
+            allResponseKeyColumnOptions={allResponseKeyColumnOptions}
+            onConditionalOperatorChange={onConditionalOperatorChange}
+            onConditionalValueChange={onConditionalValueChange}
+            conditionalValueType={conditionalValueType}
+          />
+        )}
         <ActionConfigEditor
           action={action}
           onActionConfigChange={onActionConfigChange}
