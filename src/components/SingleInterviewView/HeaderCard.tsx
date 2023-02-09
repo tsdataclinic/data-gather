56e87df62fa1300fd19098c0da6175bb175b216e
Tiffany Mei <tiffany@twosigmaventures.com>
2023-02-08 22:33:48 -0500

    change icon on header card

--- src/components/SingleInterviewView/HeaderCard.tsx
+++ src/components/SingleInterviewView/HeaderCard.tsx
@@ -1,4 +1,4 @@
-import { faGear } from '@fortawesome/free-solid-svg-icons';
+import { faTag } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import * as React from 'react';
 import { Element as ScrollableElement } from 'react-scroll';
@@ -20,7 +20,7 @@ function HeaderCard(
       className="flex w-full flex-row border border-gray-200 bg-white p-5 shadow-lg"
     >
       <div className="flex w-1/6 flex-row">
-        <FontAwesomeIcon className="h-6 w-6 pr-4" icon={faGear} />
+        <FontAwesomeIcon className="h-6 w-6 pr-4" icon={faTag} />
       </div>
       <Form ref={forwardedRef} className="w-full pr-16 pb-4">
         <Form.Input
