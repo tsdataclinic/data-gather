59f4feb52d8f1cbddf98c9cacc51988cf1bb6585
Juan Pablo Sarmiento <pablo.sarmiento@twosigma.com>
2023-10-30 19:27:00 -0700

    Updated types

--- server/models/google_sheets_settings.py
+++ server/models/google_sheets_settings.py
@@ -2,6 +2,14 @@ from typing import Literal
 
 from pydantic import BaseModel
 
+# TODO:
+# 1. Finish modeling the GoogleSheetsSettings classes
+# 2. Update the frontend model to expect this union
+# 3. Add the ability to authenticate with Google Sheets
+# 4. Load the schema
+# 5. Update the lookup config
+# 6. Update the submission actions config
+
 
 class GoogleSheetsSettings(BaseModel):
     workbooks: list[str] | None
